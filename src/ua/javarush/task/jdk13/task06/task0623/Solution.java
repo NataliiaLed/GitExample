74a039b0ba681b781fc12de52eff805e237b34d5
Nataliia <shumchenko@gmail.com>
2024-10-20 20:27:52 +0200

    Revert "JIRA-12334: Changed file configuration to support new format"
    This reverts commit da0b5110b458c36828c140a0afcd8100ef9b3859.

--- src/ua/javarush/task/jdk13/task06/task0623/Solution.java
+++ src/ua/javarush/task/jdk13/task06/task0623/Solution.java
@@ -9,7 +9,7 @@ import java.util.Scanner;
 
 public class Solution {
     public static int[][] array;//напишіть тут ваш код
-    // change 4 sdfsd
+    // change 4
     public static void main(String[] args) {
         //напишіть тут ваш код
 
